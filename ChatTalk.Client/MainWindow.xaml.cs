1451d8d2da68ca63757ccab2e5a5f922c7ac3c3c
Hyeok8598 <soonh8598@gmail.com>
2026-03-29 20:19:32 +0900

    Feature-004 : 접속 화면 구성

--- ChatTalk.Client/MainWindow.xaml.cs
+++ ChatTalk.Client/MainWindow.xaml.cs
@@ -20,5 +20,68 @@ namespace ChatTalk.Client
         {
             InitializeComponent();
         }
+
+
+        /* ===================================================================== *
+            1. Event
+         * ===================================================================== */
+        private void ConnectButton_Click(object sender, RoutedEventArgs e)
+        {
+            string serverIP = ServerIpTextBox.Text;
+            string port = PortTextBox.Text;
+            string userName = UserNameTextBox.Text;
+
+            if(!validateInputText()) return;
+
+            StatusTextBlock.Text = $"[접속 시도 중] {serverIP} : {port}";
+            StatusTextBlock.Foreground = Brushes.Orange;
+        }
+
+        private void TitleBar_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.ClickCount == 2)
+            {
+                if (this.WindowState == WindowState.Normal)
+                    this.WindowState = WindowState.Maximized;
+                else 
+                    this.WindowState = WindowState.Normal;
+            }
+
+            if (e.LeftButton == MouseButtonState.Pressed)
+            {
+                this.DragMove();
+            }
+        }
+        private void Minimize_Click(object sender, RoutedEventArgs e)
+        {
+            this.WindowState = WindowState.Minimized;
+        }
+
+        private void Close_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void CloseButton_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        /* ===================================================================== *
+            2. 사용자정의 함수
+         * ===================================================================== */
+        private bool validateInputText()
+        {
+            if (string.IsNullOrWhiteSpace(ServerIpTextBox.Text) ||
+                string.IsNullOrWhiteSpace(PortTextBox.Text)     ||
+                string.IsNullOrWhiteSpace(UserNameTextBox.Text))
+            {
+                StatusTextBlock.Text = "입력값을 확인하세요.";
+                StatusTextBlock.Foreground = Brushes.Red;
+                return false;
+            }
+
+            return true;
+        }
     }
 }
